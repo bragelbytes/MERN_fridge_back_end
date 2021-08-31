6ddc65af6230327b87a6d1ced0ee40b48b9109e1
Joe Bragel <Bragel@Joes-MBP.attlocal.net>
2021-08-31 13:50:17 -0500

    capstone server

---
+++ server.js
@@ -0,0 +1,41 @@
+//dependencies
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const session = require("express")
+const app = express()
+const db = mongoose.connection
+const itemsController = require("./controllers/reviews.js")
+require("dotenv").config()
+
+//config
+const PORT = process.env.PORT || 3003;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+//Middleware
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(express.json())
+app.use(cors())
+app.use("/items", itemsController)
+
+//Error
+db.on("error", (error) => console.log(error.message + "did you run MONGOD??");)
+db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
+db.on("disconnected", () => console.log("mongo disconnected"));
+
+app.listen(PORT, () => {
+  console.log("listening for ITEMS on port: ", PORT);
+})
+mongoose.connect(
+  MONGODB_URI,
+  {useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify:false},
+  () => {
+    console.log("connected to mongod");
+  }
+)
